9830e29f5d5083c2e23b654466443cedf2ac77e6
KajalShendage <shendagekajal123@gmail.com>
2022-08-25 23:27:18 +0530

    [Pramod] Add: Length Calculation

--- LineComparison/Program.cs
+++ LineComparison/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LineComparison;
+
+Comparison comparision = new Comparison(5, 6, 8, 9);
+comparision.CalculateLength();
